eb69d380856320ddabd7fd4b3748ba618f7063ed
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-09-04 16:14:01 +0530

    server created and running

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log('hello');
+const express = require('express');
+const { connection } = require('./config/db');
+const app = express()
+app.use(express.json())
+require('dotenv').config()
+
+const Port = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send('Welcome to User-Registration portal')
+})
+
+app.listen(Port, async () => {
+    try {
+        await connection
+        console.log("Connected to Database!");
+    } catch (error) {
+        console.log("Unable to connect DB", error);
+    }
+    console.log(`Server is running at http://localhost:${Port}`);
+})
